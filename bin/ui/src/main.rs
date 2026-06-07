9c95060b35e1de1a504a05567ae5e62c4f9fb58d
Benjamin Everest <everest.bt@gmail.com>
2026-06-07 19:38:54 +0100

    Use bevy to produce a transparent overlay

--- bin/ui/src/main.rs
+++ bin/ui/src/main.rs
@@ -1,38 +1,50 @@
-use iced::window::Settings;
-use iced::{Element, Theme, Task};
-use iced::widget::{
-    right_center, text, 
-};
+use bevy::prelude::*;
+use bevy::window::{WindowLevel, CursorOptions};
+#[cfg(any(target_os = "macos", target_os = "linux"))]
+use bevy::window::CompositeAlphaMode;
 
-pub fn main() -> iced::Result {
-    color_eyre::install().expect("Failed to install color eyre");
-    let window_settings = Settings{ maximized: true, transparent: true, ..Settings::default() };
-    iced::application(App::new, App::update, App::view)
-        .window(window_settings)
-        .theme(Theme::CatppuccinMocha)
-        .transparent(true)
-        .run()
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let window = Window {
+        // Enable transparent support for the window
+        transparent: true,
+        decorations: false,
+        window_level: WindowLevel::AlwaysOnTop,
+        #[cfg(target_os = "macos")]
+        composite_alpha_mode: CompositeAlphaMode::PostMultiplied,
+        #[cfg(target_os = "linux")]
+        composite_alpha_mode: CompositeAlphaMode::PreMultiplied,
+        ..default()
+    };
+    let cursor = CursorOptions {
+        hit_test: false,
+        ..default()
+    };
+    App::new()
+        // Make it render background as transparent
+        .insert_resource(ClearColor(Color::NONE))
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(window),
+                primary_cursor_options: Some(cursor),
+                ..default()}
+            )
+        )
+        .add_systems(Startup, setup)
+        .run();
+    Ok(())
 }
 
-#[derive(Debug, Clone)]
-enum Message {
-    
-}
-
-struct App {
-    
-}
-
-impl App {
-    fn new() -> Self {
-        Self {  }
-    }
-
-    fn update(&mut self, message: Message) -> Task<Message> {
-        Task::none()
-    }
-
-    fn view(&self) -> Element<'_, Message> {
-        text("A TEST").into()
-    }
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);    
+    commands.spawn((
+        Text::new("From an &str into a Text with the default font!"),
+        Node {
+            position_type: PositionType::Absolute,
+            bottom: px(5),
+            left: px(15),
+            ..default()
+        },
+    ));
 }
