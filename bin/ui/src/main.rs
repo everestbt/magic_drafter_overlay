09a32448282d9ca95a03ad680bfc2e1f04b7d312
Benjamin Everest <everest.bt@gmail.com>
2026-06-09 08:55:06 +0100

    Add a placeholder list

--- bin/ui/src/main.rs
+++ bin/ui/src/main.rs
@@ -3,6 +3,7 @@ use bevy::window::{CursorOptions, WindowLevel, WindowResolution};
 #[cfg(any(target_os = "macos", target_os = "linux"))]
 use bevy::window::CompositeAlphaMode;
 
+const LINE_HEIGHT: f32 = 21.;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -50,4 +51,58 @@ fn setup(mut commands: Commands) {
             ..default()
         },
     ));
+    commands.spawn((
+        Node {
+            width: percent(100),
+            height: percent(100),
+            flex_direction: FlexDirection::Row,
+            justify_content: JustifyContent::SpaceBetween,
+            ..default()
+        },
+        children![
+            vertically_scrolling_list(),
+        ],
+    ));
+}
+
+fn vertically_scrolling_list() -> impl Bundle {
+    (
+        Node {
+            flex_direction: FlexDirection::Column,
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            width: px(200),
+            ..default()
+        },
+        children![
+            (
+                // Title
+                Text::new("Vertically Scrolling List"),
+                Label,
+            ),
+            (
+                // List
+                Node {
+                    flex_direction: FlexDirection::Column,
+                    align_self: AlignSelf::Stretch,
+                    height: percent(50),
+                    overflow: Overflow::clip(),
+                    ..default()
+                },
+                Children::spawn(SpawnIter((0..15).map(move |i| {
+                    (
+                        Node {
+                            min_height: px(LINE_HEIGHT),
+                            max_height: px(LINE_HEIGHT),
+                            ..default()
+                        },
+                        children![(
+                            Text(format!("Item {i}")),
+                            Label,
+                        )],
+                    )
+                })))
+            ),
+        ],
+    )
 }
